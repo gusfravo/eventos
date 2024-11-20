ed7b14cd512e7b90a3859f064401a79bb7adf12d
Gustavo Solano <e_geperez1@stefanini.com>
2024-11-19 19:14:09 -0600

    se agrega color a tab bar

--- src/router/TabNavigator.router.tsx
+++ src/router/TabNavigator.router.tsx
@@ -4,6 +4,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // import HomeScreen from "../../screen/home.screen";
 import HomeScreen from "../screen/home/HomeScreen"
 import HeaderSection from "../shared/header/HeaderSection";
+import { WHITE_SMOKE } from "../shared/constants";
+import { backgroundStyle } from "../shared/style/background.style";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +15,10 @@ const TabNavigator = ()=>{
         screenOptions={{
           header:({ navigation, route, options }) => {
             return <HeaderSection/>;
+          },
+          tabBarStyle:{
+            backgroundColor:'#F2F2F2',
+            borderColor:'#F2F2F2'
           }
         }}>
           <Tab.Screen name="Home" component={HomeScreen} />
--- src/shared/style/background.style.ts
+++ src/shared/style/background.style.ts
@@ -3,8 +3,7 @@ import {
   } from 'react-native';
 import { WHITE_SMOKE, DEEP_GRAY } from '../constants'
 
-const isDarkMode = useColorScheme() === 'dark';
 
 export const backgroundStyle = {
-  backgroundColor: isDarkMode ? DEEP_GRAY : WHITE_SMOKE
+  backgroundColor: WHITE_SMOKE
 };
