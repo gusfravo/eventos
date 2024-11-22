d644715ed7deaf61be1c09557f68e4320439a31d
Gustavo Solano <e_geperez1@stefanini.com>
2024-11-22 15:03:43 -0600

    refactor: se agrega nueva area segura para el home de ventos

--- src/screen/home/HomeScreen.tsx
+++ src/screen/home/HomeScreen.tsx
@@ -14,7 +14,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 function HomeScreen(): React.JSX.Element {
     return (
-        <SafeAreaView style={styleHome.container}>
+        <SafeAreaView style={styleHome.container} edges={{top:'off',bottom:'off', right:'additive',left:'additive'}}>
             <SearchInput></SearchInput>
             <ListEvents></ListEvents>
         </SafeAreaView>
