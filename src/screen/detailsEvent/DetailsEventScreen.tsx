2d397ed78c25f873b7a2b7bbc7262033aa761a29
Gustavo Solano <e_geperez1@stefanini.com>
2025-01-03 16:48:14 -0600

    feat: se termina top bar de estadisticas

--- src/screen/detailsEvent/DetailsEventScreen.tsx
+++ src/screen/detailsEvent/DetailsEventScreen.tsx
@@ -4,11 +4,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { EDGES, WHITE_SMOKE } from "../../shared/constants";
 import { StyleSheet, Text } from "react-native";
 import DetailsEventStatistics from "./components/DetailsEventStatistics";
+import DetailsEventWidgets from "./components/DetailsEventWidgets";
 
 function DetailsEventScreen():React.JSX.Element{
         return (
             <SafeAreaView style={styleDetails.container} edges={EDGES}>
                 <DetailsEventStatistics></DetailsEventStatistics>
+                <DetailsEventWidgets></DetailsEventWidgets>
             </SafeAreaView>
         )
 }
@@ -19,11 +21,9 @@ export default DetailsEventScreen;
 const styleDetails = StyleSheet.create({
     container:{
         flex:1,
-        // paddingHorizontal:16,
         paddingBottom:16,
         backgroundColor:WHITE_SMOKE,
         flexDirection:'column',
         alignItems:'center',
-        rowGap:16
     }
 });
---
+++ src/screen/detailsEvent/components/DetailsEventWidgets.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function DetailsEventWidgest():React.JSX.Element{
+    return (
+        <View style={DetailsEventStyle.container}>
+
+            <View style={DetailsEventStyle.box}>
+                <View style={DetailsEventStyle.boxText}>
+                    <Text style={DetailsEventStyle.title}>234 días</Text>
+                    <Text style={DetailsEventStyle.details}>Ultima vez</Text>
+                </View>
+            </View>
+
+            <View style={DetailsEventStyle.box}>
+                <View style={DetailsEventStyle.boxText}>
+                    <Text style={DetailsEventStyle.title}>45 días</Text>
+                    <Text style={DetailsEventStyle.details}>Promedio</Text>
+                </View>
+            </View>
+
+            <View style={DetailsEventStyle.box}>
+                <View style={DetailsEventStyle.boxText}>
+                    <Text style={DetailsEventStyle.title}>8</Text>
+                    <Text style={DetailsEventStyle.details}>Eventos</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default DetailsEventWidgest;
+
+const DetailsEventStyle = StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        gap:16,
+        backgroundColor:'#FFC36F',
+        width:'100%',
+        paddingVertical:16
+    },
+    box:{
+        padding:12,
+        margin:'auto',
+        backgroundColor:'#000000',
+        opacity:.35,
+        borderRadius:10,
+        width:115,
+        height:80,
+        flexDirection:'row'
+    },
+
+    boxText:{
+        justifyContent:'center',
+        margin:'auto'
+    },
+    title:{
+        fontFamily:'Nunito Bold',
+        color:'white',
+        fontSize:14,
+        textAlign:'center'
+    },
+    details:{
+        fontFamily:'Nunito Regular',
+        fontSize:10,
+        textAlign:'center',
+        color:'white',
+    }
+});
