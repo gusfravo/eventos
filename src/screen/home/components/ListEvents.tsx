eb36b7fe4039a323c8ccf15c0d5a3f79dc7f9038
Gustavo Solano <gustavo.esp@icloud.com>
2025-02-21 21:05:07 -0600

    style: se ajustan los estilos del componente listEvent

--- src/screen/home/components/ListEvents.tsx
+++ src/screen/home/components/ListEvents.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import { View, StyleSheet, Text, FlatList } from "react-native";
+import { JSX } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
 import { WHITE_SMOKE } from "../../../shared/constants";
-import { LIST_EVENTS } from "../constants/listevents.constant";
 import ItemEvent from './ItemEvent';
-import { TabNavigatorScreenProps } from "../../../router/interfaces";
 import { ItemEventInterface } from "../interface/itemEvent.interface";
 
-function ListEvents({ navigation, data }: { navigation: any, data: ItemEventInterface[] }): React.JSX.Element {
+function ListEvents({ navigation, data }: { navigation: any, data: ItemEventInterface[] }): JSX.Element {
   return (
     <View
       style={ListEventStyle.container}>
       <FlatList
         data={data}
         renderItem={({ item }) => <ItemEvent item={item} onPress={() => {
-          console.log("Send To edit", navigation)
+          //console.log("Send To edit", navigation)
           navigation.navigate('error')
         }}></ItemEvent>}
         keyExtractor={item => item.icon}
