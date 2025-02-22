a8fa9c05eb7271482d99a22af9936c90d77b01de
Gustavo Solano <gustavo.esp@icloud.com>
2025-02-22 17:04:02 -0600

    style, se elimina recurso no utilizado

--- src/screen/newEvent/NewEventScreen.tsx
+++ src/screen/newEvent/NewEventScreen.tsx
@@ -1,7 +1,7 @@
-import {StyleSheet, ScrollView } from "react-native";
+import { StyleSheet } from "react-native";
 import { WHITE_SMOKE } from "../../shared/constants";
 import { SafeAreaView } from "react-native-safe-area-context";
-import  NewEventIcon from './components/NewEventIcon';
+import NewEventIcon from './components/NewEventIcon';
 import NewEventInputForm from "./components/NewEventInputForm";
 import NewEventDateInputForm from "./components/NewEventDateInputForm";
 import NewEventSelectColorForm from "./components/NewEventSelectColorForm";
@@ -9,30 +9,30 @@ import NewEventSelectIconForm from "./components/NewEventSelectIconForm";
 import NewEventButton from "./components/NewEventButton";
 import { EDGES } from "../../shared/constants";
 
-function NewEventScreen(){
-    return (
-        <SafeAreaView style={styleHome.container} edges={EDGES}>
-            <NewEventIcon></NewEventIcon>
-            <NewEventInputForm></NewEventInputForm>
-            <NewEventDateInputForm></NewEventDateInputForm>
-            <NewEventSelectColorForm></NewEventSelectColorForm>
-            <NewEventSelectIconForm></NewEventSelectIconForm>
-            <NewEventButton></NewEventButton>
-        </SafeAreaView>
-    );
+function NewEventScreen() {
+  return (
+    <SafeAreaView style={styleHome.container} edges={EDGES}>
+      <NewEventIcon></NewEventIcon>
+      <NewEventInputForm></NewEventInputForm>
+      <NewEventDateInputForm></NewEventDateInputForm>
+      <NewEventSelectColorForm></NewEventSelectColorForm>
+      <NewEventSelectIconForm></NewEventSelectIconForm>
+      <NewEventButton></NewEventButton>
+    </SafeAreaView>
+  );
 }
 
 export default NewEventScreen;
 
 
 const styleHome = StyleSheet.create({
-    container:{
-        flex:1,
-        paddingHorizontal:16,
-        paddingVertical:16,
-        backgroundColor:WHITE_SMOKE,
-        flexDirection:'column',
-        alignItems:'center',
-        rowGap:16
-    }
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    backgroundColor: WHITE_SMOKE,
+    flexDirection: 'column',
+    alignItems: 'center',
+    rowGap: 16
+  }
 });
