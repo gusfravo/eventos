78407585f6b13af6f128bf56216f90fc83c30f07
Gustavo Solano <gustavo.esp@icloud.com>
2025-02-26 20:27:53 -0600

    style: se elimina logs

--- src/screen/detailsEvent/components/DetailsEventList.tsx
+++ src/screen/detailsEvent/components/DetailsEventList.tsx
@@ -4,29 +4,29 @@ import { WHITE_SMOKE } from "../../../shared/constants";
 import { LIST_EVENTS } from "../../home/constants/listevents.constant";
 import DetailsEventItem from "./DetailsEventItem";
 
-function DetailsEventList():React.JSX.Element{
-    return(
-        <View style={DetailsEventListStyle.container}>
-            <FlatList
-                data={LIST_EVENTS}
-                renderItem={({ item })=> <DetailsEventItem item={item} onPress={()=>{
-                    console.log("Send To edit")
-                }}></DetailsEventItem>}
-                keyExtractor={item => item.icon}
-            ></FlatList>
-        </View>
-    )
+function DetailsEventList(): React.JSX.Element {
+  return (
+    <View style={DetailsEventListStyle.container}>
+      <FlatList
+        data={LIST_EVENTS}
+        renderItem={({ item }) => <DetailsEventItem item={item} onPress={() => {
+          //console.log("Send To edit")
+        }}></DetailsEventItem>}
+        keyExtractor={item => item.icon}
+      ></FlatList>
+    </View>
+  )
 }
 
 export default DetailsEventList;
 
 
 const DetailsEventListStyle = StyleSheet.create({
-    container:{
-        width:'100%',
-        flex:1,
-        paddingHorizontal:16,
-        paddingVertical:32,
-        backgroundColor:WHITE_SMOKE
-    }
+  container: {
+    width: '100%',
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 32,
+    backgroundColor: WHITE_SMOKE
+  }
 });
