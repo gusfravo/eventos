e129761ef59a33af48f30f2517bb325b3643ea35
Gustavo Solano <gustavo.esp@icloud.com>
2025-03-22 22:11:49 -0600

    refactor: se midifica el icono superior de la pagina crear nuevo evento

--- src/screen/newEvent/components/NewEventIcon.tsx
+++ src/screen/newEvent/components/NewEventIcon.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import IconView from "../../../shared/components/IconView/IconView";
-import { InputsForm } from "../hooks/newEventHook";
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import IconView from '../../../shared/components/IconView/IconView';
+import { InputsForm } from '../hooks/newEventHook';
 
 //TODO- Aceptar envio de cambio de icono y cambio de background
 function NewEventIcon({ inputs }: { inputs: InputsForm }): React.JSX.Element {
   return (
     <View style={eventIconsStyle.iconEventForm}>
-      <IconView
-        icon={inputs.icon ? inputs.icon : 'solar:clipboard-list-bold'}
-        iconViewOptions={{ background: inputs.color ? inputs.color : '#B4DE60', width: 96, height: 96, color: 'white' }}></IconView>
+
+      <View style={eventIconsStyle.container}>
+        <View style={eventIconsStyle.mainContainer}>
+          <IconView icon={inputs.icon ? inputs.icon : 'solar:clipboard-list-bold'} iconViewOptions={{ background: inputs.color ? inputs.color : '#B4DE60' }} />
+          <View style={[eventIconsStyle.box, eventIconsStyle.detailBox]}>
+            <Text style={eventIconsStyle.detailsBoxMainText}>{inputs.name}</Text>
+            <Text style={eventIconsStyle.detailsSoftText}>22/04/2024</Text>
+          </View>
+          <Text style={[eventIconsStyle.box, eventIconsStyle.titleDays]}>x días</Text>
+        </View>
+
+        <Text style={[eventIconsStyle.detailsSoftText]}>Ocurrencias: 1</Text>
+      </View>
+
     </View>
   );
 }
@@ -17,8 +28,50 @@ function NewEventIcon({ inputs }: { inputs: InputsForm }): React.JSX.Element {
 const eventIconsStyle = StyleSheet.create({
   iconEventForm: {
     marginVertical: 32,
-    marginHorizontal: 'auto'
-  }
-})
+    marginHorizontal: 'auto',
+    width: '80%',
+  },
+  container: {
+    height: 70,
+    marginVertical: 16,
+  },
+  mainContainer: {
+    height: 56,
+    alignContent: 'space-between',
+    flexWrap: 'wrap',
+    rowGap: 10,
+    width: '100%',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#F6F6F6',
+    padding: 8,
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+  box: {
+    width: '25%',
+  },
+  detailBox: {
+    width: '55%',
+  },
+  detailsBoxMainText: {
+    width: '100%',
+    fontFamily: 'Nunito Bold',
+    fontSize: 14,
+  },
+  detailsSoftText: {
+    width: '100%',
+    fontFamily: 'Nunito Regular',
+    fontSize: 10,
+    color: '#8E8E8E',
+    verticalAlign: 'bottom',
+    marginTop: 4,
+  },
+  titleDays: {
+    textAlign: 'right',
+    fontFamily: 'Nunito Bold',
+    fontSize: 14,
+  },
+});
 
 export default NewEventIcon;
